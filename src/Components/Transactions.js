1fe837677a5bd32d0acb61940bead91e39efa29c
ERIAN CABALLERO <eriancaballero@ERIANs-MBP.home>
2022-02-05 01:57:33 -0500

    touble shooting pt2

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -8,6 +8,20 @@ function Transactions() {
   const [transactions, setTransactions] = useState([]);
   // const [total, setTotal] = useState(0)
  
+  // useEffect(() => {
+  //   axios
+  //     .get(API_URL + "/transactions")
+  //     .then((res) => {
+  //       setTransactions(res.data);
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       throw err;
+  //     });
+
+      
+  // }, []);
+
   useEffect(() => {
     axios
       .get(API_URL + "/transactions")
@@ -36,9 +50,6 @@ function Transactions() {
 //   }
 //     setTotal(count);
 // }
-  // let totalAmount = transactions.map((transaction)=>{
-  //   return transaction.amount.reduce((a,b)=> Number(a) + Number(b), 0);
-  // })
   
   let totalAmount = transactions
     .map((transaction) => transaction.amount)
